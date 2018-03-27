c0c2ae347708de87e40e8b17efd64df567b0bbc5
aongfootlong <Aong9791>
2018-03-27 16:00:15 +0700

    End Class

--- app/src/main/java/labtest/pattrawut/androidthai/in/th/krirkshoppingmall/fragment/MainFragment.java
+++ app/src/main/java/labtest/pattrawut/androidthai/in/th/krirkshoppingmall/fragment/MainFragment.java
@@ -1,14 +1,26 @@
 package labtest.pattrawut.androidthai.in.th.krirkshoppingmall.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import labtest.pattrawut.androidthai.in.th.krirkshoppingmall.R;
+import labtest.pattrawut.androidthai.in.th.krirkshoppingmall.ServiceActivity;
+import labtest.pattrawut.androidthai.in.th.krirkshoppingmall.utility.GetAllDataByUrl;
+import labtest.pattrawut.androidthai.in.th.krirkshoppingmall.utility.MyAlert;
+import labtest.pattrawut.androidthai.in.th.krirkshoppingmall.utility.MyConstant;
 
 /**
  * Created by Pattrawut on 3/6/2018.
@@ -23,7 +35,90 @@ public class MainFragment extends Fragment {
 //        Register Controller
         registerController();
 
+//        Login Controller
+        loginController();
+
+
     }   // End Main Method
+
+    private void loginController() {
+        Button button = getView().findViewById(R.id.btnlogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText userEditText = getView().findViewById(R.id.edtUser);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+                String userString = userEditText.getText().toString().trim();
+                String passwordString = passwordEditText.getText().toString().trim();
+
+                if (userString.isEmpty() || passwordString.isEmpty()) {
+//                    Have Space
+                    MyAlert myAlert = new MyAlert(getActivity());
+                    myAlert.myDialog(getString(R.string.title_have_space),
+                            getString(R.string.message_have_space));
+
+                } else {
+//                    No Space
+                    try {
+
+                        MyConstant myConstant = new MyConstant();
+                        boolean statusAboolean = true;
+                        GetAllDataByUrl getAllDataByUrl = new GetAllDataByUrl(getActivity());
+                        getAllDataByUrl.execute(myConstant.getURLGetAllUserString());
+
+                        String jsonString = getAllDataByUrl.get();
+                        Log.e("27marcsv1", "Json==>" + jsonString);
+
+                        JSONArray jsonArray = new JSONArray(jsonString);
+
+                        String[] ColumnString = myConstant.getColumnUser();
+                        String[] LoginString = new String[ColumnString.length];
+
+                        for (int i=0; i<jsonArray.length();i+=1) {
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+                            if (userString.equals(jsonObject.getString(ColumnString[2]))) {
+
+                                statusAboolean = false;
+                                for (int i1=0;i1<ColumnString.length; i1+=1) {
+                                    LoginString[i1] = jsonObject.getString(ColumnString[i1]);
+                                }
+                            }
+                        }
+
+                        if (statusAboolean) {
+//                            User False
+                            MyAlert myAlert = new MyAlert(getActivity());
+                            myAlert.myDialog("User False", "No This User In My Database");
+
+                        } else if (passwordString.equals(LoginString[3])) {
+//                            Pass True
+                            Toast.makeText(getActivity(), "Welcome" + LoginString[1],
+                                    Toast.LENGTH_SHORT).show();
+
+//                            Move to Service Activity
+                            Intent intent = new Intent(getActivity(), ServiceActivity.class);
+                            intent.putExtra("Login", LoginString);
+                            startActivity(intent);
+                            getActivity().finish();
+
+                        } else {
+//                            Pass False
+                            MyAlert myAlert = new MyAlert(getActivity());
+                            myAlert.myDialog("Password False","Please Try Again");
+                        }
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+
+                }   // If
+
+            }
+        });
+    }
 
     private void registerController() {
         TextView textView = getView().findViewById(R.id.txtNewRegister);
--- app/src/main/java/labtest/pattrawut/androidthai/in/th/krirkshoppingmall/utility/MyConstant.java
+++ app/src/main/java/labtest/pattrawut/androidthai/in/th/krirkshoppingmall/utility/MyConstant.java
@@ -1,5 +1,7 @@
 package labtest.pattrawut.androidthai.in.th.krirkshoppingmall.utility;
 
+import java.net.URI;
+
 /**
  * Created by Pattrawut on 3/27/2018.
  */
@@ -9,8 +11,21 @@ public class MyConstant {
 //    About URL
 
     private String URLAddUserString = "http://androidthai.in.th/kir/addDataUng.php";
+    private String URLGetAllUserString = "http://androidthai.in.th/kir/getAllUserAong.php";
+
+    //    Array
+    private String[] ColumnUser = new String[]{"id", "Name", "User", "Password", "Mode"};
+
+    public  String[] getColumnUser() {
+        return ColumnUser;
+    }
+
+    public String getURLGetAllUserString() {
+        return getURLAddUserString();
+    }
 
     public String getURLAddUserString() {
         return URLAddUserString;
     }
+
 }   // Main Class
